12fbbd7429b5f95eb8b19edf1b2fb70457412f1e
debrasbread <figueroa.debra@gmail.com>
2023-02-02 20:19:49 -0600

    CardValidation B

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import validator from './validator.js';
+
 function validate() {
-  console.log("validate: " + validate);
-  const cardNumber=document.getElementById("cardnumber").value;
-  console.log("cardNumber: " + cardNumber);
-  if(cardNumber.length===0||/^\s+$/.test(cardNumber)||isNaN(cardNumber)) {
-    document.getElementById("introNumber").innerHTML = "Por favor, introduce un número.";
+
+  const cardNumber = document.getElementById("cardnumber").value;
+  const nombre = document.getElementById("name").value;
+
+  document.getElementById("introNumber").innerHTML = "";
+  document.getElementById("valida").innerHTML = "";
+  document.getElementById("invalida").innerHTML = "";
+  document.getElementById("masked-card").innerHTML = "";
+  document.getElementById("introName").innerHTML = "";
+
+  if (!/^[a-zA-Z\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\s]+$/.test(nombre)) {
+    document.getElementById("introName").innerHTML = "Por favor, introduce tu nombre.";
+    return;
+  }
+
+  if (cardNumber.length === 0 || /^\s+$/.test(cardNumber) || isNaN(cardNumber)) {
+    document.getElementById("introNumber").innerHTML = "Por favor, introduce un número de tarjeta.";
     return;
   }
   else {
-    const result = validator.isValid(cardNumber);// estaba fuera de else
+    const result = validator.isValid(cardNumber);
     if (result === true) {
-      document.getElementById("valida").innerHTML = "Tarjeta inválida";
-        
-        
+      document.getElementById("valida").innerHTML = "Tarjeta válida" + "<p></p>" + "¡Compra exitosa!";
       const maskedCard = validator.maskify(cardNumber);
       document.getElementById("masked-card").innerHTML = maskedCard;
     }
     else {
       document.getElementById("invalida").innerHTML = "Tarjeta inválida";
-        
-
     }
   }
-}
 
+  document.getElementById("cardnumber").value = "";
+
+}
 
 const btnValidar = document.getElementById("click");
 btnValidar.addEventListener("click", validate);
--- src/validator.js
+++ src/validator.js
@@ -2,50 +2,42 @@ const validator = {
   isValid, maskify,
 };
 function isValid(cardNumber) {
-  console.log("isValid: ", cardNumber);
   const array = cardNumber.split("").map(Number).reverse();
-  console.log("array + split + number + reverse: ", array);
   let sumPares = 0;
   let sumImpares = 0;
   for (let i = 0; i < array.length; i++) {
-    console.log("bucle for: ", i);
     if (i % 2 === 1) {
-      console.log("determino pares: ");
       const prodPares = array[i] * 2;
       if (prodPares >= 10) {
-        console.log("prodpares>=10: ");
         sumPares += Math.floor(prodPares / 10) + (prodPares % 10);
       }
       else {
-        console.log("prodPares<10: ");
         sumPares += prodPares;
       }
     }
     else {
-      console.log("Impares: ");
       sumImpares += array[i];
     }
   }
-  console.log("sumImpares: ", sumImpares);
-  console.log("sumPares: ", sumPares);
   const sumTotal = sumImpares + sumPares;
-  console.log("sumTotal: ", sumTotal);
 
   if (sumTotal % 10 === 0) {
-    console.log("Múltiplo de 10, válido: ");
     return true;
   }
   else {
-    console.log("No es múltiplo de 10, inválido: ");
     return false;
   }
 }
-function maskify(cardNumber) {
-  console.log("maskify(cardNumber): ", cardNumber);
-  const last4Digits = cardNumber.slice(-4);
-  console.log("últimos 4 dígitos de la tarjeta:", last4Digits);
-  const maskedNumber = "".padStart(cardNumber.length - 4, "#");
-  console.log("Número enmascarado:", maskedNumber);
-  return maskedNumber + last4Digits;
+
+function repeatString(str, num) {
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+  return result;
 }
+function maskify(cardNumber) {
+  return repeatString("#", cardNumber.length - 4) + cardNumber.slice(-4);
+}
+
 export default validator;
